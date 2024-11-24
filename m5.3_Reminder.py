97f11e0996436a66e75510ce791375cfdb7e7a8d
Марина <m030882@gmail.com>
2024-11-24 15:19:02 +0300

    Добавлена Fx установки времени -напоминание

--- m5.3_Reminder.py
+++ m5.3_Reminder.py
@@ -5,6 +5,21 @@ import datetime
 import time
 import pygame
 
+def set():
+    rem = sd.askstring("Время напоминания", "Установите напоминание в 24 ч. формате ЧЧ:ММ")
+    if rem:
+        try:
+            hour_p = int(rem.split(":")[0])
+            minute_p = int(rem.split(":")[1])
+            now = datetime.datetime.now()
+            print(now)
+            dt = now.replace(hour= hour_p, minute=minute_p)
+            print(dt)
+            t_p = dt.timestamp()
+            print(t_p)
+        except Exception as e:
+            mb.showerror("Ошибка", "Произошла ошибка {e}" )
+
 window = Tk()
 window.title ("Напомню")
 Label = Label(text = "Установите напоминание")
